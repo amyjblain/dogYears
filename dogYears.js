58b149cda02630b47df62e46ecadfe230ce85e8f
Amy <39568291+amyjblain@users.noreply.github.com>
2020-02-13 20:15:00 -0500

    Add files via upload

---
+++ dogYears.js
@@ -0,0 +1,8 @@
+var myAge = 36;
+var earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+var myAgeInDogYears = earlyYears + laterYears;
+myName = "Amy".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
